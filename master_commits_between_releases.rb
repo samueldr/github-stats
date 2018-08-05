bc9c1ae5fbca0680f3ec18cceb3be34e398e29e7
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-08-05 18:48:15 -0400

    Adds more stats.

--- master_commits_between_releases.rb
+++ master_commits_between_releases.rb
@@ -2,18 +2,48 @@
 
 require "bundler/setup"
 require_relative "db"
+require "shellwords"
 require "json"
 require "pp"
-
-pull_commits = $db.execute("SELECT sha FROM pull_commits;").map { |row| row.first }
-
-
-pull_commits.concat(merge_commit_shas)
-
 
 # I hate to hardcode this, but I want to work in this project's CWD.
 # May instead look for an environment variable or config file.
 REPO_LOCATION = File.join(ENV["HOME"], "tmp", "nixpkgs", "nixpkgs")
+
+# Operates on the given repo location
+def git(*cmd)
+  Dir.chdir(REPO_LOCATION) do
+    #puts " $ #{[:git].concat(cmd).shelljoin}"
+    `#{[:git].concat(cmd).shelljoin}`
+  end
+end
+
+# Given a `sha` found in the repository, tries to find a pull request with the
+# commit. A found commit gives a low-confidence plausibility that it was
+# rebased+pushed instead of merged.
+def find_pull_commit_for(sha)
+  data = {
+    raw_body: "%B",
+    #subject: "%s",
+  }.map do |k, format|
+    [
+      k,
+      git(:show, sha, "--no-patch", "--format=#{format}").strip
+    ]
+  end.to_h
+
+  $db.execute("SELECT data FROM pull_commits WHERE raw_body = ?", data[:raw_body]).map do |(data)|
+    JSON.parse(data, symbolize_names: true)
+  end
+  # This gets more records. MUCH SLOWER. From 30 seconds to 9 minutes 15 seconds.
+  # TODO : figure out if the data is *right*, if it ise, figure out a faster way.
+  #$db.execute("SELECT data FROM pull_commits WHERE raw_body LIKE ?", "%" + data[:raw_body] + "%").map do |(data)|
+  #  JSON.parse(data, symbolize_names: true)
+  #end
+end
+
+pull_commits = $db.execute("SELECT sha FROM pull_commits;").map { |row| row.first }
+
 merge_commit_shas = $db.execute("SELECT merge_commit_sha FROM pulls WHERE merge_commit_sha NOT NULL").map(&:first)
 
 pull_commits.concat(merge_commit_shas)
@@ -56,12 +86,20 @@ end
 
 ranges.each do |(a,b)|
   commits = get(a, b)
-  cherry = cherry_picked(a, b)
   difference = commits - pull_commits
   puts "#{a}..#{b}: #{difference.count} commits on master, (#{percentage(difference.count, commits.count)}%) #{commits.count} total commits."
+
+  cherry = cherry_picked(a, b)
   difference = difference - cherry
   puts "Removing cherry-pick commits (#{cherry.count})"
   puts "#{a}..#{b}: #{difference.count} commits on master, (#{percentage(difference.count, commits.count)}%) #{commits.count} total commits."
+
+  possible_rebases = difference.select do |sha|
+    find_pull_commit_for(sha).length > 0
+  end
+  difference = difference - possible_rebases
+  puts "Removing possibly rebased commits (#{possible_rebases.count})"
+  puts "#{a}..#{b}: #{difference.count} commits on master, (#{percentage(difference.count, commits.count)}%) #{commits.count} total commits."
+
   puts ""
-  #pp difference
 end
