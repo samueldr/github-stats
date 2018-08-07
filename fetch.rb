1560a7de2ba40bbe0c1e130e01d9fcdcde9ba1be
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-08-06 23:45:43 -0400

    Adds basic "update only new stuff".

--- fetch.rb
+++ fetch.rb
@@ -28,6 +28,9 @@ user.login
 puts "Starting..."
 #$client.auto_paginate = true
 
+
+latest_update = $db.execute("SELECT updated_at FROM pulls ORDER BY updated_at DESC LIMIT 1").first.first
+
 # https://developer.github.com/v3/pulls/#list-pull-requests
 $client.pulls(
   "nixos/nixpkgs",
@@ -47,6 +50,18 @@ loop do
     DB.replace_pull(pull)
     $client.get(pull.commits_url).map do |commit|
       DB.replace_commit(number, commit)
+    end
+
+    # FIXME : argument parsing
+    # This is a bit "dangerous" as with a failed partial refresh it would leave
+    # a new "updated_at" value with a gap in the data.
+    # The real solution is to instead reverse the direction (asc) and limit the
+    # search query using >= latest_update
+    if ARGV.first == "--update" then
+      if pull[:updated_at].to_s < latest_update then
+        puts "Finished with updates..."
+        exit 0
+      end
     end
   end
 
